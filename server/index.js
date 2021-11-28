550876c272e35023bb8bd63831c4598ee79526ba
abhinaya-r <abhinaya.raghu@gmail.com>
2021-11-27 21:21:57 -0500

    update postbuild

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const path = require('path');
 
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/src')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
@@ -19,7 +19,7 @@ app.get("/api", (req, res) => {
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
