8f4dd828f8ac0655fa78a77836f30d3ada7e0ff3
ishanikulkarni <51721555+ishanikulkarni@users.noreply.github.com>
2021-11-27 22:51:19 -0500

    landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 // client/src/App.js
 
-import React, {useState} from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as BrowserRouter, Route, Routes } from "react-router-dom";
-import LandingPage from "./components/landing_page";
-// import LoginPage from "./components/login";
-// import SignupPage from "./components/signup";
-// import MainPage from "./components/mainpage"
-// import RecommendationPage from "./components/recommendations"
-// import AboutPage from "./components/about"
-
+import {
+  BrowserRouter as BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import LandingPage from "./pages/landing_page";
+// import LoginPage from "./pages/login";
+// import SignupPage from "./pages/signup";
+// import MainPage from "./pages/mainpage"
+// import RecommendationPage from "./pages/recommendations"
+// import AboutPage from "./pages/about"
 
 function App() {
   const [data, setData] = React.useState(null);
@@ -21,17 +24,17 @@ function App() {
       .then((data) => setData(data.message));
   }, []);
 
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>{!data ? "Loading..." : data}</p>
-    //   </header>
-    // </div>
-    console.log(data)
+  // <div className="App">
+  //   <header className="App-header">
+  //     <img src={logo} className="App-logo" alt="logo" />
+  //     <p>{!data ? "Loading..." : data}</p>
+  //   </header>
+  // </div>
+  console.log(data);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage/>} />
+        <Route path="/" element={<LandingPage />} />
         {/* <Route path="/login" element={<LoginPage/>} />
         <Route path="/signup" element={<SignupPage/>} />
         <Route path="/dashboard" element={<MainPage/>} />
@@ -39,7 +42,7 @@ function App() {
               element={<RecommendationPage/>}/>
             <Route exact path="/about" element={<AboutPage/>} /> */}
       </Routes>
-  </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
