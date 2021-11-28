7e272cac0d58a7d1dab79ea4d8c01d1680c7201f
abhinaya-r <abhinaya.raghu@gmail.com>
2021-11-27 20:40:47 -0500

    change files path

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const path = require('path');
 
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/src')));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
