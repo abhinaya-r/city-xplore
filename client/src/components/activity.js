34f129695773ed6fbb5e492b449fb347cf607d96
ishanikulkarni <51721555+ishanikulkarni@users.noreply.github.com>
2021-12-20 11:22:23 -0500

    fixed sign up page

--- client/src/components/activity.js
+++ client/src/components/activity.js
@@ -1,62 +1,53 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import Grid from "@material-ui/core/Grid";
-import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardContent from "@material-ui/core/CardContent";
 import Box from "@mui/material/Box";
-import axios from "axios"
+import RefreshIcon from "@mui/icons-material/Refresh";
+import Grid from "@material-ui/core/Grid";
 
 export default function Activity(props) {
+  const gridStyle = {
+    border: "0px",
+    marginTop: "0px",
+    marginBottom: "0px",
+  };
+
+  const boxStyle = {
+    width: "100%",
+    color: "#919E6A",
+    fontSize: "10px",
+    fontWeight: "bold",
+    padding: "5px",
+    marginTop: "-20px",
+    marginBottom: "10px",
+  };
 
   return (
     <div>
-        <Box
+      <Box
         component="span"
         sx={{
-            display: "block",
-            bgcolor: "#FFF6F1",
-            borderRadius: "10px",
-            marginBottom:"30px"
+          display: "block",
+          bgcolor: "#FFF6F1",
+          borderRadius: "10px",
+          marginBottom: "30px",
         }}
-        >
-            <Box sx= {{alignItems: 'left'}} style={{
-            width: "50%",
-            color: "#919E6A",
-            fontSize: "10px",
-            fontWeight: "bold",
-            padding: "5px",
-            marginTop: "-20px",
-            marginBottom: "10px",
-            }}>
-            {props.name}
+      >
+        <Grid container spacing={0} columns={2}>
+          <Grid item xs={11} style={gridStyle}>
+            <Box sx={{ alignItems: "left" }} style={boxStyle}>
+              {props.name}
             </Box>
-            <Box sx= {{ alignItems: 'right'}} style={{
-            width: "50%",
-            color: "#919E6A",
-            fontSize: "10px",
-            fontWeight: "bold",
-            padding: "5px",
-            marginTop: "-20px",
-            marginBottom: "10px",
-            }}>
-            {props.rating}
+            <Box sx={{ alignItems: "left" }} style={boxStyle}>
+              {props.rating} stars
             </Box>
-            <Box sx= {{}} style={{
-            width: "100%",
-            color: "#919E6A",
-            fontSize: "10px",
-            fontWeight: "bold",
-            padding: "5px",
-            marginTop: "-20px",
-            marginBottom: "10px",
-            }}>
-            {props.address}
+            <Box sx={{ alignItems: "left" }} style={boxStyle}>
+              {props.address}
             </Box>
-        
-        </Box>
-
+          </Grid>
+          {/* <Grid item xs={1} style={gridStyle}>
+            <RefreshIcon sx={{ color: "#919E6A" }}></RefreshIcon>
+          </Grid> */}
+        </Grid>
+      </Box>
     </div>
   );
 }
