620259a557de78d67a385ad543114b481d94a28c
abhinaya-r <abhinaya.raghu@gmail.com>
2022-01-07 23:35:02 -0500

    remove code

--- db.sql
+++ db.sql
@@ -12,7 +12,7 @@ CREATE TABLE [IF NOT EXISTS] users (
 CREATE TABLE itineraries (
    user_id INTEGER,
    itin_id INTEGER GENERATED ALWAYS AS IDENTITY,
-   itinerary JSON [],
+   itinerary JSONB [],
    date DATE NOT NULL
 );
 
